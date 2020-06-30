1774632f00a6feecbd1a932b34e99fb87b4c0724
Armando <Tapia>
2020-06-30 13:53:18 -0500

    [ADD]: some information about my experience

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
   show_git: boolean = false;
+  show_node: boolean = false;
+  show_angular: boolean = false;
+  show_mongo: boolean = false;
+  show_aws: boolean = false;
+  show_goodPractices: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -14,6 +20,21 @@ export class AboutMeComponent implements OnInit {
     switch (tool) {
       case 'git':
         this.show_git = !this.show_git;
+        break;
+      case 'node':
+        this.show_node = !this.show_node;
+        break;
+      case 'angular':
+        this.show_angular = !this.show_angular;
+        break;
+      case 'mongo':
+        this.show_mongo = !this.show_mongo;
+        break;
+      case 'aws':
+        this.show_aws = !this.show_aws;
+        break;
+      case 'goodPractices':
+        this.show_goodPractices = !this.show_goodPractices;
         break;
     }
   }
